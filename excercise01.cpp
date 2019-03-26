b1092b8e64a2a01210cee232c72c0bbedbfc5db9
putrinabila <43230324+putrinabila@users.noreply.github.com>
2019-03-26 11:00:09 +0700

    Update excercise01.cpp

--- excercise01.cpp
+++ excercise01.cpp
@@ -7,77 +7,113 @@ Tanggal Pemvbuatan : 26 Maret 2019
 Deksripsi : jawaban excercise 5
 */
 
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-struct Pembalap{
-    int nomor;
-    char nama;
-    int waktu;
-    Pembalap* next;
-    Pembalap* prev;
+struct Pembalap {
+   int nomor;
+   char nama;
+   int waktu;
+   Pembalap* next;
+   Pembalap* prev;
 };
 
-	void createNode(Pembalap* &p, int nomor, char nama){
-		p=new Pembalap;
-		p->nama=nama;
-		p->nomor=nomor;
-			cout<<"Masukkan waktu : "; cin>>p->waktu;
-		p->next=NULL;
-		p->prev=NULL;
+void createNode(Pembalap* &p,  int nomor, char nama){
+  p = new Pembalap;
+  cout<<"Nomor : ";cin>>p->nomor;
+  cout<<"Nama  : ";cin>>p->nama;
+  cout<<"Waktu :";cin>p->waktu;
+  p->next=NULL;
+  p->prev=NULL;
 }
-		void insertFirst(Pembalap* &head, Pembalap* node){
-    	if (head==NULL){
-        head=node;
-    }
-    else {
-        node->next=head;
-        head=node;
-    }
+
+void insertFirst(Pembalap* &head, Pembalap* node){
+  if (head==NULL){
+   head=node;
+  }
+ 
+  else{
+    node->next=head;
+    head->prev=node;
+    head=node;
+  }
 }
-	void search(Pembalap* &firstNode, int nomorKey, Pembalap* &pToUpdate){
-   		 pToUpdate=firstNode;
-    while(pToUpdate->nomor!=nomorKey){
-        pToUpdate=pToUpdate->next;
+
+
+void insertBefore(Pembalap* &head, int nomorKey, Pembalap* node){
+  Pembalap pBantu;
+  while (pBantu!=NULL){
+    if(pBantu->nomor==nomorkey){
+      if(pBantu->prev==NULL){
+      insertFirst(head,node);
+     }
+   else {
+     node->next=pBantu;
+     node->prev=pBantu->prev;
+     pBantu->prev->next=node;
+     pBantu->prev=node;
+    
     }
+   pBantu=pBantu->next;
+   }
+  }
 }
-	void insertBefore(Pembalap* &head, int nomorKey, Pembalap* node){
-    	Pembalap* searching;
-    	search(head,nomorKey,searching);
-    	if (head==NULL){
-        head=node;
-    }
-    		else {
-        		node->next=searching;
-        		node->prev=searching->prev;
-        		searching->prev->next=node;
-    		    searching->prev=node;   
-    }
-}
+
 
 void deleteByKey(Pembalap* &head, int nomorKey, Pembalap* &deletedNode){
+  Pembalap pBantu;
+  pBantu=head;
+  while(pBantu!=NULL){
+    if(pBantu->prev==nomorkey){
+      if(pBantu->next==NULL){
+        Pembalap last;
+        last=head;
+    while(last->next)
+   }
+  }
+ }
 }
 
-void traversal(Pembalap* head){
-    Pembalap* help;
-    if (head==NULL){
-        cout<<"***LIST KOSONG***\n";
-    }
-    else {
-        help=head;
-        while(help!=NULL){
-            cout<<"Nomor: "<<help->nomor<<endl;
-            cout<<"Nama: "<<help->nama<<endl;
-            cout<<"Waktu: "<<help->waktu<<endl;
-            cout<<endl;
-            help=help->next;
-        }
-    }
+
+void search(Pembalap* &firstNode, int nomorKey, Pembalap* &pToUpdate){
+ 
 }
-// sorting ascending
-void sortingByNomor(Pembalap* &head);
-// update Pembalap from user input
-void update(Pembalap* firstNode, int nomorKey);
+
+
+void traversal(Pembalap* head){
+ Pembalap* help;
+ if (head==NULL){
+  cout<<"***LIST KOSONG***\n";
+ }
+ else {
+  help=head;
+  while(help!=NULL){
+   cout<<"Nomor : "<<help->nomor<<endl;
+   cout<<"Nama  : "<<help->nama<<endl;
+   cout<<"Waktu : "<<help->waktu<<endl;
+   cout<<endl;
+   help=help->next;
+  }
+ }
+}
+
+
+void sortingByNomor(Pembalap* &head){
+  Pembalap pBantu;
+  pBantu=head;
+  while(pBantu!=NULL){
+    if(pBantu->nomor > pBantu->next->nomor){
+   
+    }
+  }
+}
+
+
+void update(Pembalap* firstNode, int nomorKey){
+ 
+}
+
+
 
 int main() {
  Pembalap* head = NULL;
@@ -89,29 +125,30 @@ int main() {
    createNode(pBaru, i+1, nama++);
    insertFirst(head, pBaru);
  }
- cout << "\n>>> List" << endl;
+
+ cout << ">>> List" << endl;
  traversal(head);
+
  int keyNomor = 2;
- cout << "\n>>> Insert Before nomor " << keyNomor << endl;
+
+ cout << ">>> Insert Before nomor " << keyNomor << endl;
  createNode(pBaru, 99, 'z');
  insertBefore(head, keyNomor, pBaru); 
  traversal(head);
+ cout << ">>> SortingByNomor" << endl;
+ sortingByNomor(head);
+ traversal(head);
+
+ keyNomor = 3;
+ cout << ">>> Delete nomor " << keyNomor << endl;
+ Pembalap* pHapus = NULL;
+ deleteByKey(head, keyNomor, pHapus);
+ traversal(head);
+
+ keyNomor = 4;
+ cout << ">>> Update nomor " << keyNomor << endl;
+ update(head, keyNomor);
+
+ cout << ">>> Updated list\n";
+ traversal(head);
 }
-/*
-//  cout << "\n>>> SortingByNomor" << endl;
-//  sortingByNomor(head);
-//  traversal(head);
-
-//  keyNomor = 3;
-//  cout << "\n>>> Delete nomor " << keyNomor << endl;
-//  Pembalap* pHapus = NULL;
-//  deleteByKey(head, keyNomor, pHapus);
-//  traversal(head);
-
-//  keyNomor = 4;
-//  cout << "\n>>> Update nomor " << keyNomor << endl;
-//  update(head, keyNomor);
-
-//  cout << "\n>>> Updated list\n";
-//  traversal(head);
- //belum selesai 
